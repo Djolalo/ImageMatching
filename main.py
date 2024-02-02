c347394030eb00e7aea85b5a59f5052fbaf14fa1
djilali.bahous.etu <djilali.bahous.etu@univ-lille.fr>
2024-02-02 16:06:05 +0100

    question 1 exo1

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+img = cv.imread('box.png', cv.IMREAD_GRAYSCALE)
+# Constructor default params: nOctaveLayers = 3,
+# contrastThreshold = 0.04, edgeThreshold = 10, sigma = 1.6
+sift = cv.SIFT_create() # construct
+kp = sift.detect(img) # detect keypoints
+img_with_kp = cv.drawKeypoints(img, kp, img, None,\
+cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+plt.imshow(img_with_kp)
+plt.show()
